547250bc706d702dda7410dd2ab5f6cc7f7a9ae8
Azerothian <admin@illisian.com.au>
2017-01-09 17:32:17 +1000

    start of dropbox

--- src/web/ui/components/placeholder.jsx
+++ src/web/ui/components/placeholder.jsx
@@ -35,49 +35,58 @@ const placeholderTarget = {
     }
     return {osNode};
   },
-  hover(props, monitor, component) {
-    const dragIndex = monitor.getItem().index;
-    const hoverIndex = props.index;
+  hover({osNode}, monitor, component) {
+    if (monitor.isOver({ shallow: true })) {
+      const dropItem = monitor.getItem();
+      if(dropItem.osNode) {// existing item 
+        
+      }
+      //console.log("hover!", {item, osNode, monitor, component});
 
-    // Don"t replace items with themselves
-    if (dragIndex === hoverIndex) {
-      return;
+      // const  = 
     }
+    
+    // const hoverIndex = props.index;
 
-    // Determine rectangle on screen
-    const hoverBoundingRect = findDOMNode(component).getBoundingClientRect();
+    // // Don"t replace items with themselves
+    // if (dragIndex === hoverIndex) {
+    //   return;
+    // }
 
-    // Get vertical middle
-    const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+    // // Determine rectangle on screen
+    // const hoverBoundingRect = findDOMNode(component).getBoundingClientRect();
 
-    // Determine mouse position
-    const clientOffset = monitor.getClientOffset();
+    // // Get vertical middle
+    // const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
 
-    // Get pixels to the top
-    const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+    // // Determine mouse position
+    // const clientOffset = monitor.getClientOffset();
 
-    // Only perform the move when the mouse has crossed half of the items height
-    // When dragging downwards, only move when the cursor is below 50%
-    // When dragging upwards, only move when the cursor is above 50%
+    // // Get pixels to the top
+    // const hoverClientY = clientOffset.y - hoverBoundingRect.top;
 
-    // Dragging downwards
-    if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
-      return;
-    }
+    // // Only perform the move when the mouse has crossed half of the items height
+    // // When dragging downwards, only move when the cursor is below 50%
+    // // When dragging upwards, only move when the cursor is above 50%
 
-    // Dragging upwards
-    if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
-      return;
-    }
+    // // Dragging downwards
+    // if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+    //   return;
+    // }
 
-    // Time to actually perform the action
-    props.moveCard(dragIndex, hoverIndex);
+    // // Dragging upwards
+    // if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+    //   return;
+    // }
 
-    // Note: we"re mutating the monitor item here!
-    // Generally it"s better to avoid mutations,
-    // but it"s good here for the sake of performance
-    // to avoid expensive index searches.
-    monitor.getItem().index = hoverIndex;
+    // // Time to actually perform the action
+    // props.moveCard(dragIndex, hoverIndex);
+
+    // // Note: we"re mutating the monitor item here!
+    // // Generally it"s better to avoid mutations,
+    // // but it"s good here for the sake of performance
+    // // to avoid expensive index searches.
+    // monitor.getItem().index = hoverIndex;
   }
 };
 
